e9fe7c1069906c5b7435f14991b8dfb2cc023238
krislibaeer <krislibaeer@googlemail.com>
2013-05-30 11:39:04 +0200

    some changes
    Change-Id: I6a3e197e56f30127f857ee2f59124ebd069f0e5b

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -71,7 +71,7 @@ TARGET_arm_CFLAGS :=    -O3 \
                         -fstrict-aliasing    \
                         -fno-tree-vectorize
 else
-TARGET_arm_CFLAGS :=    -Os \
+TARGET_arm_CFLAGS :=    -O2  \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops
@@ -89,7 +89,7 @@ ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
                             -fno-tree-vectorize
     else
     TARGET_thumb_CFLAGS :=  -mthumb \
-                            -O2 \
+                            -Os \
                             -fomit-frame-pointer \
                             -fno-strict-aliasing
     endif
